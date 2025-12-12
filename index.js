9c94fe65699ee1094e39b3b8386aef142b55f086
zone95181 <151130361+zone95181@users.noreply.github.com>
2025-12-12 21:08:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
- import { createServer } from "http";
- import { Server } from "socket.io";
- import express from "express";
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import express from 'express';
 
- const app = express();
- const http = createServer(app);
- const io = new Server(http);
+const app = express();
+const http = createServer(app);
+const io = new Server(http);
 
- // 静的ファイルを配置するディレクトリを指定
- app.use(express.static("public"));
+app.use(express.static('public'));
 
- io.on("connection", (socket) =>
-   socket.onAny((event, data) => socket.broadcast.emit(event, data)),
- );
+io.on('connection', (socket) =>
+  socket.onAny((event, data) => socket.broadcast.emit(event, data))
+);
 
- http.listen(Number(process.env.PORT) || 3000);
+http.listen(Number(process.env.PORT) || 3000);
