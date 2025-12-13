53c3c16f6a6b7ce4d91a14deeaa97d8269b09d2e
zone95181 <151130361+zone95181@users.noreply.github.com>
2025-12-13 09:08:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
 // server.js
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port : 3001});
 
-wss.on("connection", ws => {
-  ws.on("message", msg => {
-    // 受け取ったメッセージを他の全クライアントに送る
-    wss.clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(smg.toString());
-      }
-    });
-  });
-});
-
-console.log("Signaling Server running on ws://localhost:3001");
+node serfer.js
