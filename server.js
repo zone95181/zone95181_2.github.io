457f6349c3189e632b52194af0b720babdb5a48a
zone95181 <151130361+zone95181@users.noreply.github.com>
2025-12-13 09:19:18 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 // server.js
+const WebSocket = require("ws");
 
-node serfer.js
+// ポート 3001 で WebSocket サーバーを起動
+const wss = new WebSocket.Server({ port: 3001 });
+
+console.log("Signaling Server running on ws://localhost:3001");
+wss.on("connection", ws => { console.log("Client connected");
+
+// クライアントからメッセージが来たら他の全クライアントへ送信
+ws.on("message", msg => { console.log("Received:", msg.toString());
+  wss.clients.forEach(client => {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      client.send(msg.toString());
+      }
+    });
+  });
+
+  // 切断時
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
